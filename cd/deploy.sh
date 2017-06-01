6a2fe016cae6c4f8570bddc0034ad491d07a3575
jeroen wijdemans <jeroen.wijdemans@gmail.com>
2017-06-01 18:37:14 +0200

    add encrypted deploy keys

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo 'deploy ... '
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    # add openssl cmd here ...
+    openssl aes-256-cbc -K $encrypted_e3157f88fb5c_key -iv $encrypted_e3157f88fb5c_iv -in codesigning.asc.enc -out codesigning.asc -d
     gpg --fast-import ./cd/codesigning.asc
     ./gradlew uploadArchives -s
     echo "Finished uploading archives"
